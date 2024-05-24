9f1cc6b11f38fce13a6166ece8761fad3d475e1d
Clément Touzet pc portable maison <clement.touzet40@gmail.com>
2024-05-24 12:06:10 +0200

    modified text

--- src/users/users.tsx
+++ src/users/users.tsx
@@ -13,7 +13,8 @@ const Users = ({ users, createUser, deleteUser, updateUser }: Props) => {
   return (
     <div className="max-w-xl mt-12">
       <p className="mb-8 font-bold">
-        Cas normal (avec un useState) pour gérer la liste des utilisateurs
+        Cas normal (avec un useState) pour gérer la liste des utilisateurs pour
+        voir le cas avec le reducer, il faut aller sur l'autre branche git
       </p>
       <NewUserForm createUser={createUser} />
       <h1 className="h1 font-bold text-3xl mb-2">Liste des utilisateurs</h1>
